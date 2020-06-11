a858d4159a71a66a3841709a22d3530ec01b1014
Lucas TAILLEBOURG <lucas.taillebourg@gmail.cm>
2020-06-11 16:20:59 -0700

    Front  : Ajout de bootstrap + modif page latest

--- anima_newsletter_front-end/src/app/app.module.ts
+++ anima_newsletter_front-end/src/app/app.module.ts
@@ -7,6 +7,7 @@ import {RouterModule} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 import {CommonModule} from '@angular/common';
 import {MobxAngularModule} from 'mobx-angular';
+import {SharedModule} from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {MobxAngularModule} from 'mobx-angular';
     CommonModule,
     HttpClientModule,
     RouterModule.forRoot([]),
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- anima_newsletter_front-end/src/app/shared/shared.module.ts
+++ anima_newsletter_front-end/src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {MobxAngularModule} from 'mobx-angular';
+import {MenuComponent} from './menu/menu.component';
 
 @NgModule({
   imports: [
@@ -17,7 +18,8 @@ import {MobxAngularModule} from 'mobx-angular';
     FormsModule,
     ReactiveFormsModule,
     MobxAngularModule,
+    MenuComponent
   ],
-  declarations: []
+  declarations: [MenuComponent]
 })
 export class SharedModule {}
