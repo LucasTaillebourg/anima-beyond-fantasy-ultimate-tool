83dee98d0599ba5e536afd012381f2f3c064efc1
Hugo <Durzoo52@gmail.com>
2020-06-24 19:09:38 +0200

    Back : Add Rest GetAllNewsByAuthor

--- anima_newsletter_backend/src/main/java/com/anima/features/newsletter/NewsRepository.java
+++ anima_newsletter_backend/src/main/java/com/anima/features/newsletter/NewsRepository.java
@@ -2,6 +2,7 @@ package com.anima.features.newsletter;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collection;
 
@@ -12,6 +13,9 @@ public interface NewsRepository extends CrudRepository<NewsEntity, Integer> {
 
     @Query(value = "SELECT * FROM NEWS ORDER BY creation_date DESC LIMIT 1", nativeQuery = true)
     NewsEntity findLastNews();
+
+    @Query(value = "SELECT * FROM NEWS WHERE LOWER(author) = LOWER(:author) ORDER BY creation_date DESC", nativeQuery = true)
+    Collection<NewsEntity> getAllNewsByAuthor(@RequestParam( value = "author") String author);
 
 
 }
