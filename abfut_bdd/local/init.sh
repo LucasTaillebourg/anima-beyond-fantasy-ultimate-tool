98b7e9c78831058ec9b826461cb72132c59a85e3
Hugo <durzoo52@gmail.com>
2020-07-17 14:05:09 -0700

    ADD Liquibase

--- abfut_bdd/local/init.sh
+++
@@ -1,6 +0,0 @@
-docker cp init.sql docker-mysql:/init.sql
-docker cp init.sql docker-mysql:/initwithabout.sql
-docker cp init.sql docker-mysql:/initwithaboutwhilerunning.sql
-docker exec -i docker-mysql mysql -u root -proot -e 'show databases;'
-docker exec -i docker-mysql mysql -u root -proot -e 'use test;'
-docker exec -i docker-mysql mysql -u root -proot < init.sql
